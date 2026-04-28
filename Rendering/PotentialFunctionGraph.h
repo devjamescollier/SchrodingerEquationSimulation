f1e67c86cf80800339a34bae8f6622231ed9bc79
James Collier <jwcollie@usc.edu>
2026-04-28 16:09:25 -0700

    Add PropagationGraph class for potential function rendering

---
+++ Rendering/PotentialFunctionGraph.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Math/WaveFunction.h"
+#include "../Math/Function.h"
+
+class PropagationGraph {
+public:
+    PropagationGraph(WaveFunction* waveFunc, Function* potential, double dt);
+
+    void   Advance();
+    void   Draw() const;
+    double GetTime() const;
+
+private:
+    WaveFunction* waveFunction;
+    Function*     potential;
+    double        dt;
+    double        time;
+
+    void drawFilledCurve(double xMin, double xMax, double yMax) const;
+    void drawAxes(double xMin, double xMax, double yMax) const;
+    void drawText(double x, double y, const char* text) const;
+
+    static void hsvToRgb(float h, float& r, float& g, float& b);
+};
