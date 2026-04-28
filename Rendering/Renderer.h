67df9bc0c4efbafb213ec9fdb91d1eeb461a159c
James Collier <jwcollie@usc.edu>
2026-04-28 16:08:34 -0700

    Add Renderer class for graphical rendering

---
+++ Rendering/Renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "PropagationGraph.h"
+
+class Renderer {
+public:
+    static void Init(int argc, char** argv, int width, int height, const char* title);
+    static void SetGraph(PropagationGraph* graph);
+    static void Run();
+
+private:
+    static PropagationGraph* graph;
+    static int               windowWidth;
+    static int               windowHeight;
+
+    static void display();
+    static void reshape(int w, int h);
+    static void timer(int value);
+};
